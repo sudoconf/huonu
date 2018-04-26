4b39fb94935b162494a6d6befce2f283dfb60597
huxiao <1017098134@qq.com>
2018-04-26 14:37:29 +0800

    fix

--- huonu-web/backend/modules/report/views/default/create.php
+++ huonu-web/backend/modules/report/views/default/create.php
@@ -492,7 +492,7 @@ use yii\helpers\Url;
             <label for="lastname" class="col-sm-3 control-label">选择定向人群</label>
             <div class="col-sm-9">
                 <ul class="list-group" id="addhtml">
-                    <li class="control-group"><input type="checkbox"><span>全选</span></li>
+                    <li class="control-group"><input type="checkbox" id="select-all"><span>全选</span></li>
                 </ul>
             </div>
         </div>
@@ -628,13 +628,10 @@ use yii\helpers\Url;
             url: 'ajax-get-target.html',
             type: 'post',
             dataType: 'json',
-            data: {
-                'page': ''
-            },
             success: function (res) {
                 console.log(res)
                 for (var i = 0; i < res.data.length; i++) {
-                    htmls += '<li class="control-group"><input type="checkbox" value="' + res.data[i].taobao_user_id + '"><span>' + res.data[i].taobao_user_nick + '</span></li>'
+                    htmls += '<li class="control-group"><input class="check-box" type="checkbox" value="' + res.data[i].taobao_user_id + '"><span>' + res.data[i].taobao_user_nick + '</span></li>'
                 }
                 $('#addhtml').append(htmls)
                 layer.open({
@@ -660,5 +657,24 @@ use yii\helpers\Url;
 
     });
 
+    // 失败的愿意是英文我这里是动态加载的
+    // $('#select-all').click(function () {
+    //     //全选按钮选中状态，索引0取document对象
+    //     var selectAll = $(this)[0].checked;
+    //     console.log(selectAll);
+    //     //操作所有复选框的选中状态
+    //     var checkbox = $(".check-box");
+    //     console.log(checkbox);
+    //     checkbox.prop("checked", selectAll);
+    // })
+    $(document).on('click', '#select-all', function () {
+        var checkbox = $(".check-box");
+        if ($(this).is(':checked')) {
+            checkbox.prop("checked", true);
+        } else {
+            checkbox.prop("checked", false);
+        }
+
+    })
 
 </script>
